b52e32369dd205f0a745a39e1f5f781ac0aa8caf
Nicoly Mendes <111599909+NyMendes@users.noreply.github.com>
2023-10-17 22:00:23 -0300

    Add files for correction

---
+++ assets/js/click.js
@@ -0,0 +1,6 @@
+// const playerButton = document.querySelector('#play-music')
+// const play = document.querySelector('#playButton')
+
+// playerButton.onclick = function () {
+//     play.classList.toggle('fa-play');
+// }
